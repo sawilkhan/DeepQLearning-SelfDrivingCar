54926e4a2c348967702697e1a8a288acd3c00295
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2023-07-08 14:54:27 +0530

    Initial Commit
    Uploaded Project Files

---
+++ Self_Driving_Car/ai2.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 13:13:07 2021
+
+@author: Sawil
+"""
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+#creating the architechture of the neural network
+
+class Network(nn.Module):
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+    
+#implementing the experience replay
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+            
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+    
+    
+class Dqn():
+    def __init__(self, input_size, nb_action, gamma):
+            self.gamma = gamma
+            self.reward_window = []
+            self.model = Network(input_size, nb_action)
+            self.memory = ReplayMemory(100000)
+            self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+            self.last_state = torch.Tensor(input_size).unsqueeze(0)
+            self.last_action = 0
+            self.last_reward = 0
+            
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile=True))*7) #temperature = 7
+        action = probs.multinomial()
+        return action.data[0,0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma*next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph = True)
+        self.optimizer.step()
